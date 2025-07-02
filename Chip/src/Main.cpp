87b0d4e667afb7382496157496a3b91dc3464351
dabusmc <shakierdevyt@gmail.com>
2025-07-03 00:14:36 +0100

    Screen Texture

--- Chip/src/Main.cpp
+++ Chip/src/Main.cpp
@@ -4,96 +4,95 @@
 #include <SDL.h>
 #undef main
 
-#define SCREEN_WIDTH    800
-#define SCREEN_HEIGHT   600
+#define SCREEN_WIDTH    64
+#define SCREEN_HEIGHT   32
 
 int main()
 {
-    // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        std::cout << "SDL could not be initialized!" << std::endl
-            << "SDL_Error: " << SDL_GetError() << std::endl;
-        return 0;
-    }
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		SDL_Log("Unable to intialize SDL: %s", SDL_GetError());
+		return 1;
+	}
 
-    // Create window
-    SDL_Window* window = SDL_CreateWindow("CHIP-8",
-        SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,
-        SCREEN_WIDTH, SCREEN_HEIGHT,
-        SDL_WINDOW_SHOWN);
-    
-    if (!window)
-    {
-        std::cout << "Window could not be created!" << std::endl
-            << "SDL_Error: " << SDL_GetError() << std::endl;
-    }
-    else
-    {
-        // Create renderer
-        SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-        if (!renderer)
-        {
-            std::cout << "Renderer could not be created!" << std::endl
-                << "SDL_Error: " << SDL_GetError() << std::endl;
-        }
-        else
-        {
-            // Declare rect of square
-            SDL_Rect squareRect;
+	SDL_Window* window = SDL_CreateWindow("CHIP-8",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		SCREEN_WIDTH * 20,
+		SCREEN_HEIGHT * 20,
+		SDL_WINDOW_SHOWN);
 
-            // Square dimensions: Half of the min(SCREEN_WIDTH, SCREEN_HEIGHT)
-            squareRect.w = std::min(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-            squareRect.h = std::min(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
+	if (window == nullptr)
+	{
+		SDL_Log("Unable to create SDL window: %s", SDL_GetError());
+		return 1;
+	}
 
-            // Square position: In the middle of the screen
-            squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
-            squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-            // Event loop exit flag
-            bool quit = false;
+	if (renderer == nullptr)
+	{
+		SDL_Log("Unable to create SDL renderer: %s", SDL_GetError());
+		return 1;
+	}
 
-            // Event loop
-            while (!quit)
-            {
-                SDL_Event e;
+	SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-                // Wait indefinitely for the next available event
-                SDL_WaitEvent(&e);
+	SDL_Texture* texture = SDL_CreateTexture(
+		renderer,
+		SDL_PIXELFORMAT_RGBA32,
+		SDL_TEXTUREACCESS_STREAMING,
+		SCREEN_WIDTH,
+		SCREEN_HEIGHT
+	);
 
-                // User requests quit
-                if (e.type == SDL_QUIT)
-                {
-                    quit = true;
-                }
+	if (texture == nullptr)
+	{
+		SDL_Log("Unable to create texture: %s", SDL_GetError());
+	}
 
-                // Initialize renderer color white for the background
-                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	uint8_t pixels[SCREEN_WIDTH * SCREEN_HEIGHT * 4] = { 0 };
+	for (auto i = 0; i < SCREEN_WIDTH * 4; i += 4)
+	{
+		pixels[12 * SCREEN_WIDTH + i + 0] = 255;
+		pixels[12 * SCREEN_WIDTH + i + 1] = 255;
+		pixels[12 * SCREEN_WIDTH + i + 2] = 255;
+		pixels[12 * SCREEN_WIDTH + i + 3] = 255;
+	}
 
-                // Clear screen
-                SDL_RenderClear(renderer);
+	int texturePitch = 0;
+	void* texturePixels = nullptr;
+	if (SDL_LockTexture(texture, nullptr, &texturePixels, &texturePitch) != 0)
+	{
+		SDL_Log("Unable to lock texture: %s", SDL_GetError());
+	}
+	else
+	{
+		memcpy(texturePixels, pixels, texturePitch * SCREEN_HEIGHT);
+	}
+	SDL_UnlockTexture(texture);
 
-                // Set renderer color red to draw the square
-                SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
+	bool loop = true;
+	SDL_Event e;
+	while (loop)
+	{
+		while (SDL_PollEvent(&e) != 0)
+		{
+			if (e.type == SDL_QUIT)
+			{
+				loop = false;
+			}
+		}
 
-                // Draw filled square
-                SDL_RenderFillRect(renderer, &squareRect);
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+		SDL_RenderPresent(renderer);
+	}
 
-                // Update screen
-                SDL_RenderPresent(renderer);
-            }
+	SDL_DestroyTexture(texture);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 
-            // Destroy renderer
-            SDL_DestroyRenderer(renderer);
-        }
-
-        // Destroy window
-        SDL_DestroyWindow(window);
-    }
-
-    // Quit SDL
-    SDL_Quit();
-
-    return 0;
+	return 0;
 }
